f2333c200bd047df8acbf28f7ae049fd80284b14
amina1000 <amina1000@yandex.ru>
2021-03-21 18:49:39 +0300

    Урок 4.Создание чата

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+
 public class Controller {
+    @FXML public TextArea textAria;
+    @FXML public javafx.scene.control.TextField textField;
+
+    public void sendMSG(ActionEvent actionEvent) {
+        textAria.appendText(textField.getText() + '\n');
+        textField.setText("");
+    }
 }
